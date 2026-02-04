5df4cbee8fff0d48d6599e6cf5c50aa485f3b15d
Clay <claystonebarongo@gmail.com>
2026-02-04 23:07:46 +0300

    daily updates

--- 8a_time.js
+++ 8a_time.js
@@ -1,9 +1,9 @@
 //THIS PRINTS TIME AFTER EVERY SECOND
 
-// setInterval(() => {
-//     const now = new Date();
-//     console.log(now.toLocaleTimeString());
-// }, 1000);
+setInterval(() => {
+    const now = new Date();
+    console.log(now.toLocaleTimeString());
+}, 1000);
 
 
 //THIS dISPLAYS ONLT DATE
