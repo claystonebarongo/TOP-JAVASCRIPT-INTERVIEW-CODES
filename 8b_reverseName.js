f0b003b05d9a41d8875d81f98e148d1fd562a658
Clay <claystonebarongo@gmail.com>
2026-02-04 23:13:04 +0300

    daily updates

---
+++ 8b_reverseName.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name? ", function (name) {
+
+    let reversed = "";
+
+    for (let i = name.length - 1; i >= 0; i--) {
+        reversed += name[i];
+    }
+
+    console.log("Reversed name: " + reversed);
+
+    rl.close();
+});
