3f01fc36920fecf035bbc7a98e34426ac243ef4f
Clay <claystonebarongo@gmail.com>
2026-02-06 22:03:13 +0300

    daily updates

---
+++ 10-toughtjs.js
@@ -0,0 +1,16 @@
+function flatten(arr) {
+    let result = [];
+
+    arr.forEach((item) => {
+        if (Array.isArray(item)) {
+            // If it's an array, recursive call and spread the results
+            result.push(...flatten(item));
+        } else {
+            result.push(item);
+        }
+    });
+
+    return result;
+}
+
+// Modern "Cheat" way: arr.flat(Infinity);
