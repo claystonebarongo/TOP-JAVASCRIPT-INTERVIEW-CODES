c2ebdc98d3429ae31e599ac54b8c21a3c49b8ae8
Clay <claystonebarongo@gmail.com>
2026-02-09 13:32:31 +0300

    daily updates

---
+++ coercion.js
@@ -0,0 +1,35 @@
+console.log(2 + "2");
+console.log(2 - "2");
+console.log("5" * "3");
+console.log("5" + 3);
+
+//OUTPUT LINE 1 IS 22 ----JavaScript sees a number + string
+
+//It converts the number to a string
+
+//Performs string concatenation
+
+
+//OUTPUT LINE 2 IS 0       The - operator only works with numbers
+
+//JavaScript converts "2" (string) → 2 (number)
+
+//Then performs subtraction: 2 - 2 = 0   
+
+
+//OUTPUT LINE 3 IS 15
+// * forces numeric conversion
+
+// Both strings become numbers
+
+// 5 * 3 = 15
+
+// OUPUT LINE 4 IS 53
+
+// + with a string always means concatenation
+
+// Number 3 becomes string "3"
+
+// Result → "53"
+
+
