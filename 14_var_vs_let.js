42d47dec50fc0006ee66e5fc6721d3be62489cc2
Clay <claystonebarongo@gmail.com>
2026-02-09 14:15:14 +0300

    daily updates

---
+++ 14_var_vs_let.js
@@ -0,0 +1,27 @@
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i)
+    }, 1000)
+
+}
+
+// var has function scope, not block scope
+
+// The loop finishes before setTimeout runs
+
+// By the time the callback executes, i has already become 5
+
+// All callbacks reference the same variable i
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i)
+    }, 1000)
+
+}
+
+// Why it works?
+
+// let has block scope
+
+// Each loop iteration gets its own i
