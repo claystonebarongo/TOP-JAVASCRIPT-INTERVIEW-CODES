517f169494e8031c42f2f3ab1f8a526b09904379
Clay <claystonebarongo@gmail.com>
2026-04-08 23:12:45 +0300

    daily updates

--- 4_remove_duplicate_elements_from_array.js
+++ 4_remove_duplicate_elements_from_array.js
@@ -2,9 +2,12 @@ function removeDuplicates(arr) {
     let unique = [];
 
     for (let i = 0; i < arr.length; i++) {
+
+
         if (!unique.includes(arr[i])) {
             unique.push(arr[i]);
         }
+
     }
 
     return unique;
