eecc30626c4cc6257de1a71bc9c58eb266ef73d4
Clay <claystonebarongo@gmail.com>
2026-04-20 21:13:18 +0300

    daily updates

--- 12_superhero.js
+++ 12_superhero.js
@@ -1,6 +1,8 @@
 async function getHeroNames() {
     try {
         const response = await fetch('https://akabab.github.io/superhero-api/api/all.json');
+
+
         const heroes = await response.json();
 
         heroes.forEach(hero => {
