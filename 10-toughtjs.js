b4126d3c2675cc64c74ece668dea44ad79ab6716
Clay <claystonebarongo@gmail.com>
2026-05-07 23:00:50 +0300

    daily updates

--- 10-toughtjs.js
+++ 10-toughtjs.js
@@ -2,6 +2,8 @@ function flatten(arr) {
     let result = [];
 
     arr.forEach((item) => {
+
+
         if (Array.isArray(item)) {
             // If it's an array, recursive call and spread the results
             result.push(...flatten(item));
